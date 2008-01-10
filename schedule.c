5766508bcdbdb9f6576f0ac9cb192459e2c1f393
Heikki Orsila <heikki.orsila@iki.fi>
2008-01-10 23:58:43 +0200

    Report exit status for jobs [PERFECTIVE]

--- schedule.c
+++ schedule.c
@@ -23,6 +23,22 @@
 #define MAX_CMD_SIZE 65536
 
 
+enum job_result {
+	JOB_SUCCESS,
+	JOB_FAILURE
+};
+
+struct job_ack {
+	int place;
+	enum job_result result;
+};
+
+struct executionplace {
+	size_t jobnumber;
+	int busy;
+};
+
+
 static pid_t polling_fork(void)
 {
 	pid_t child;
@@ -41,10 +57,62 @@ static pid_t polling_fork(void)
 }
 
 
-static void run(const char *cmd, int ps, int fd)
+static int read_job_ack(int fd, struct executionplace *places, int nplaces)
+{
+	struct job_ack joback;
+	ssize_t ret;
+	struct executionplace *place;
+
+	ret = read(fd, &joback, sizeof joback);
+	if (ret < 0) {
+		if (errno == EAGAIN || errno == EINTR)
+			return 0;
+
+		die("read %zd: %s)\n", ret, strerror(errno));
+	} else if (ret == 0) {
+		die("Job queue pipe was broken\n");
+	}
+
+	if (ret != sizeof(joback))
+		die("Unaligned read: returned %zd\n", ret);
+
+	assert(joback.place < nplaces);
+
+	place = &places[joback.place];
+
+	place->busy = 0;
+
+	if (verbosemode)
+		fprintf(stderr, "Job %zd finished %s\n", place->jobnumber,
+			joback.result == JOB_SUCCESS ?
+			"successfully" : "unsuccessfully");
+
+	return 1;
+}
+
+
+static void write_job_ack(int fd, struct job_ack joback, const char *cmd)
+{
+	ssize_t ret;
+
+	/* pipe(7) guarantees write atomicity when sizeof(ps) <= PIPE_BUF */
+	assert(sizeof(joback) <= PIPE_BUF);
+
+	ret = write(fd, &joback, sizeof joback);
+	if (ret < 0)
+		die("PS %d job ack failed: %s\n", joback.place, cmd);
+
+	if (ret != sizeof(joback))
+		die("Unaligned write: returned %zd\n", ret);
+}
+
+
+static void run(const char *cmd, int ps, size_t jobnumber, int fd)
 {
 	ssize_t ret;
 	char run_cmd[MAX_CMD_SIZE];
+	struct job_ack joback = {.place = ps,
+	                         .result = JOB_FAILURE};
 
 	if (executionplace) {
 		ret = snprintf(run_cmd, sizeof run_cmd, "%s %d", cmd, ps + 1);
@@ -52,24 +120,23 @@ static void run(const char *cmd, int ps, int fd)
 		ret = snprintf(run_cmd, sizeof run_cmd, "%s", cmd);
 	}
 
-	if (ret >= sizeof(run_cmd))
+	if (ret >= sizeof(run_cmd)) {
+		write_job_ack(fd, joback, run_cmd);
 		die("Too long a command: %s\n", cmd); /* cmd, not run_cmd */
+	}
 
 	if (verbosemode)
-		fprintf(stderr, "EXECUTE: %s\n", run_cmd);
+		fprintf(stderr, "Job %zd execute: %s\n", jobnumber, run_cmd);
 
 	ret = system(run_cmd);
-	if (ret == -1)
+	if (ret == -1) {
+		write_job_ack(fd, joback, run_cmd);
 		die("job delivery failed: %s\n", run_cmd);
+	}
 
-	/* pipe(7) guarantees write atomicity when sizeof(ps) <= PIPE_BUF */
-	assert(sizeof(ps) <= PIPE_BUF);
+	joback.result = JOB_SUCCESS;
 
-	ret = write(fd, &ps, sizeof ps);
-	if (ret < 0)
-		die("PS %d job ack failed: %s\n", ps, run_cmd);
-
-	assert(ret == sizeof(ps));
+	write_job_ack(fd, joback, run_cmd);
 }
 
 
@@ -98,10 +165,10 @@ static ssize_t read_stripped_line(char *buf, size_t buflen, FILE *f)
 }
 
 
-void schedule(int nprocesses)
+void schedule(int nplaces)
 {
 	char cmd[MAX_CMD_SIZE];
-	char *busy;
+	struct executionplace *places;
 	int pindex;
 	int p[2];
 	ssize_t ret;
@@ -114,43 +181,27 @@ void schedule(int nprocesses)
 	if (jobfile == NULL)
 		exitmode = 1; /* If no valid files, go to exitmode */
 
-	assert(nprocesses > 0);
+	assert(nplaces > 0);
 
 	if (pipe_closeonexec(p))
 		die("Can not create a pipe: %s\n", strerror(errno));
 
-	/* All processing stations are not busy in the beginning -> calloc() */
-	busy = calloc(nprocesses, 1);
-	if (busy == NULL)
+	/* All processing stations are non-busy in the beginning -> calloc() */
+	places = calloc(nplaces, sizeof(places[0]));
+	if (places == NULL)
 		die("No memory for process array\n");
 
 	while (1) {
 		/* Find a free execution place */
-		for (pindex = 0; pindex < nprocesses; pindex++) {
-			if (!busy[pindex])
+		for (pindex = 0; pindex < nplaces; pindex++) {
+			if (!places[pindex].busy)
 				break;
 		}
 
-		if (pindex == nprocesses ||
-		    (exitmode && jobsdone < jobsread)) {
+		if (pindex == nplaces || (exitmode && jobsdone < jobsread)) {
 
-			ret = read(p[0], &pindex, sizeof pindex);
-			if (ret < 0) {
-				if (errno == EAGAIN || errno == EINTR)
-					continue;
-
-				die("read %zd: %s)\n", ret, strerror(errno));
-			} else if (ret == 0) {
-				die("Job queue pipe was broken\n");
-			}
-
-			if (ret != sizeof(pindex))
-				die("Unaligned read: returned %zd\n", ret);
-
-			/* Note: pindex has a new value from previous read() */
-			busy[pindex] = 0;
-
-			jobsdone++;
+			if (read_job_ack(p[0], places, nplaces))
+				jobsdone++;
 
 			continue;
 		}
@@ -176,15 +227,17 @@ void schedule(int nprocesses)
 		if (ret == 0 || cmd[0] == '#')
 			continue;
 
+		places[pindex].busy = 1;
+		places[pindex].jobnumber = jobsread;
+
 		jobsread++;
-		busy[pindex] = 1;
 
 		if (polling_fork() == 0) {
 			/* Close some child file descriptors */
 			close(0);
 			close(p[0]);
 
-			run(cmd, pindex, p[1]);
+			run(cmd, pindex, places[pindex].jobnumber, p[1]);
 
 			exit(0);
 		}
