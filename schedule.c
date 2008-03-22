dolfly/jobqueue

commit d3a57b6ffaf7b56d0037c2ff568e10c6738d29ff

--max-retries=x sets maximum number of retries for failed jobs [PERFECTIVE]

schedule.c
@@ -33,10 +33,11 @@ enum job_result {
 struct job {
 	size_t jobnumber;
 	char *cmd;
+	int retries;
 };
 
 struct job_ack {
-	size_t jobnumber;
+	struct job *job;
 	int place;
 	enum job_result result;
 };
@@ -49,7 +50,31 @@ struct executionplace {
 
 static struct vplist failedjobs = VPLIST_INITIALIZER;
 
-static struct vplist runningjobs = VPLIST_INITIALIZER;
+static void free_job(struct job *job)
+{
+	free(job->cmd);
+	job->cmd = NULL;
+
+	job->jobnumber = -1;
+	job->retries = -1;
+
+	free(job);
+}
+
+
+static int test_job_restart(struct job *job)
+{
+	if (job->retries < requeuefailedjobs) {
+		job->retries++;
+
+		if (vplist_append(&failedjobs, job))
+			die("Out of memory: can not requeue failed job\n");
+
+		return 0;
+	}
+
+	return 1;
+}
 
 
 static void read_job_ack(size_t *jobsdone, int fd,
@@ -58,10 +83,8 @@ static void read_job_ack(size_t *jobsdone, int fd,
 	struct job_ack joback;
 	ssize_t ret;
 	struct executionplace *place;
-	struct vplist *listnode;
-	struct job *job;
-	int found;
 	char *machine;
+	int jobdone;
 
 	ret = read(fd, &joback, sizeof joback);
 	if (ret < 0) {
@@ -104,38 +127,22 @@ static void read_job_ack(size_t *jobsdone, int fd,
 		place->jobsrunning--;
 
 	if (requeuefailedjobs) {
-
-		if (joback.result == JOB_SUCCESS) {
-			(*jobsdone)++;
-		} else {
-			found = 0;
-			job = NULL;
-
-			VPLIST_FOR_EACH(listnode, &runningjobs) {
-				job = listnode->item;
-
-				if (job->jobnumber == joback.jobnumber) {
-					found = 1;
-					break;
-				}
-			}
-
-			assert(found);
-
-			/* Remove the job from runningjobs, and
-			 *  add it to failedjobs to restart it later */
-			if (vplist_remove_item(&runningjobs, job))
-				die("BUG: running jobs item not found\n");
-
-			if (vplist_append(&failedjobs, job))
-				die("Out of memory: can not requeue failed job\n");
-		}
+		if (joback.result == JOB_SUCCESS)
+			jobdone = 1;
+		else
+			jobdone = test_job_restart(joback.job);
 	} else {
+		/* jobdone is TRUE in no-restart mode */
+		jobdone = 1;
+	}
+
+	if (jobdone) {
+		free_job(joback.job);
 		(*jobsdone)++;
 	}
 
 	if (VERBOSE)
-		fprintf(stderr, "Job %zd finished %s\n", joback.jobnumber,
+		fprintf(stderr, "Job %zd finished %s\n", joback.job->jobnumber,
 			joback.result == JOB_SUCCESS ?
 			"successfully" : "unsuccessfully");
 }
@@ -165,9 +172,6 @@ static struct job *read_job(FILE **jobfile, size_t *jobsread)
 
 	job = vplist_pop_head(&failedjobs);
 	if (job != NULL) {
-		if (vplist_append(&runningjobs, job))
-			die("Out of memory: can not re-add job\n");
-
 		/* Do not increase *jobsread for restarted jobs */
 		return job;
 	}
@@ -195,13 +199,11 @@ static struct job *read_job(FILE **jobfile, size_t *jobsread)
 		die("Can not allocate memory for job: %s\n", cmd);
 
 	*job = (struct job) {.jobnumber = *jobsread,
+			     .retries = 0,
 			     .cmd = strdup(cmd)};
 
 	if (job->cmd == NULL)
 		die("Can not allocate memory for cmd: %s\n", cmd);
-
-	if (vplist_append(&runningjobs, job))
-		die("Can not append job to running list: %s\n", cmd);
 
 	(*jobsread)++;
 
@@ -209,11 +211,11 @@ static struct job *read_job(FILE **jobfile, size_t *jobsread)
 }
 
 
-static void run(const struct job *job, int ps, int fd)
+static void run(struct job *job, int ps, int fd)
 {
 	ssize_t ret;
 	char cmd[MAX_CMD_SIZE];
-	struct job_ack joback = {.jobnumber = job->jobnumber,
+	struct job_ack joback = {.job = job,
 				 .place = ps,
 	                         .result = JOB_FAILURE};
 	char *machine;

TO_DO
@@ -6,9 +6,6 @@ List of features to implement:
 
 	* randomize node assignment
 
-	* re-run for failed jobs
-		* maximum number of retries (add warning to documentation)
-
 	* task graph scheduling
 		* pure simulation mode: do only scheduling
 

jobqueue.c
@@ -40,8 +40,8 @@ static struct vplist jobfilenames = VPLIST_INITIALIZER;
 static const char *USAGE =
 "\n"
 "SYNTAX:\n"
-"\tjobqueue [-e] [-n x] [-m list] [-r] [-v] [--version] [-x n]\n"
-"\t         [FILE ...]\n"
+"\tjobqueue [-e] [-n x] [-m list] [--max-restart=x] [-r] [-v] [--version]\n"
+"\t         [-x n] [FILE ...]\n"
 "\n"
 "jobqueue is a tool for executing lists of jobs on several processors or\n"
 "machines in parallel. jobqueue reads jobs (shell commands) from files. If no\n"
@@ -65,11 +65,15 @@ static const char *USAGE =
 "    execution place as a name for the job. Also, this option implies \"-n x\",\n"
 "    where x is the number of names read from the file.\n"
 "\n"
+" --max-restarts=x, implies -r / --restart-failed, but sets the maximum number\n"
+"    of restarts for each job\n"
+"\n"
 " -r / --restart-failed, if a job that is executed returns an error code, it is\n"
-"    restarted (possibly on other execution place). If the error code is 1, the\n"
+"    restarted (on some execution place). If the error code is 1, the\n"
 "    job simply failed and it is restarted. If the error code is 2, the\n"
 "    execution place is marked as being failed, and thus, no additional jobs\n"
-"    will be started on that node.\n"
+"    will be started on that node. WARNING: There is no limit for maximum\n"
+"    number of restarts unless --max-restart is used.\n"
 "\n"
 " -v / --verbose, enter verbose mode. Print each command that is executed.\n"
 "\n"
@@ -232,18 +236,20 @@ int main(int argc, char *argv[])
 		OPT_EXECUTION_PLACE = 'e',
 		OPT_HELP            = 'h',
 		OPT_MACHINE_LIST    = 'm',
+		OPT_MAX_RESTART     = 1000,
 		OPT_NODES           = 'n',
 		OPT_RESTART_FAILED  = 'r',
-		OPT_MAXISSUE        = 'x',
+		OPT_MAX_ISSUE       = 'x',
 		OPT_VERBOSE         = 'v',
-		OPT_VERSION         = 1000,
+		OPT_VERSION         = 1001,
 	};
 
 	const struct option longopts[] = {
 		{.name = "help",            .has_arg = 0, .val = OPT_HELP},
 		{.name = "execution-place", .has_arg = 0, .val = OPT_EXECUTION_PLACE},
 		{.name = "machine-list",    .has_arg = 1, .val = OPT_MACHINE_LIST},
-		{.name = "max-issue",       .has_arg = 1, .val = OPT_MAXISSUE},
+		{.name = "max-issue",       .has_arg = 1, .val = OPT_MAX_ISSUE},
+		{.name = "max-restart",     .has_arg = 1, .val = OPT_MAX_RESTART},
 		{.name = "nodes",           .has_arg = 1, .val = OPT_NODES},
 		{.name = "restart-failed",  .has_arg = 0, .val = OPT_RESTART_FAILED},
 		{.name = "verbose",         .has_arg = 0, .val = OPT_VERBOSE},
@@ -282,7 +288,8 @@ int main(int argc, char *argv[])
 			break;
 
 		case 'r':
-			requeuefailedjobs = 1;
+			if (!requeuefailedjobs)
+				requeuefailedjobs = INT_MAX;
 			break;
 
 		case 'v':
@@ -296,6 +303,15 @@ int main(int argc, char *argv[])
 				die("Invalid parameter: -x %s\n", optarg);
 
 			maxissue = l;
+			break;
+
+		case OPT_MAX_RESTART:
+			l = strtol(optarg, &endptr, 10);
+
+			if ((l < 0) || l >= INT_MAX || *endptr != 0)
+				die("Invalid parameter: %s\n", optarg);
+
+			requeuefailedjobs = l;
 			break;
 
 		case OPT_VERSION:

selftests/selftest.sh
@@ -2,19 +2,20 @@
 
 com=../jobqueue
 
-n=1000
+n=500
 m=10
 
-echo "Running $n jobs in $m processing stations (takes 10 secs or so)"
+echo "Running $n jobs in $m processing stations"
 
 yes true |head -n $n |$com -n $m
 
-echo "Running deadlock test"
-for i in $(seq 25) ; do
-    yes ./randomfailure.py |head -n1 |$com -n2
-done
+function deadlocktest() {
+    echo "Running deadlock test with" "$@"
+    for i in $(seq 25) ; do
+	yes ./randomfailure.py |head -n1 |$com -n2 "$@"
+    done
+}
 
-echo "Running deadlock test with -r"
-for i in $(seq 25) ; do
-    yes ./randomfailure.py |head -n1 |$com -n2 -r
-done
+deadlocktest
+deadlocktest -r
+deadlocktest --max-restart=1

commit 5958b264b05d174f0e6b063b7194bb6267d29448

Fix bug that caused freed memory to be read [CORRECTIVE]
This bug was introduced in commit
d3a57b6ffaf7b56d0037c2ff568e10c6738d29ff, the commit that
added --max-retries=x option.

schedule.c
@@ -136,15 +136,16 @@ static void read_job_ack(size_t *jobsdone, int fd,
 		jobdone = 1;
 	}
 
-	if (jobdone) {
-		free_job(joback.job);
-		(*jobsdone)++;
-	}
-
 	if (VERBOSE)
 		fprintf(stderr, "Job %zd finished %s\n", joback.job->jobnumber,
 			joback.result == JOB_SUCCESS ?
 			"successfully" : "unsuccessfully");
+
+	if (jobdone) {
+		free_job(joback.job);
+		joback.job = NULL;
+		(*jobsdone)++;
+	}
 }
 
 