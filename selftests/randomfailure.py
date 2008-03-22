ec4920ef093742d1c0873b9597a138e4ecc2822b
Heikki Orsila <heikki.orsila@iki.fi>
2008-03-22 19:22:30 +0200

    Add test case for randomly failing jobs [PREVENTIVE]

---
+++ selftests/randomfailure.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import random
+import sys
+
+sys.exit(random.randint(0, 1))
--- selftests/selftest.sh
+++ selftests/selftest.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+com=../jobqueue
+
 n=1000
 m=10
 
 echo "Running $n jobs in $m processing stations (takes 10 secs or so)"
 
-yes true |head -n $n |../jobqueue -n $m
+yes true |head -n $n |$com -n $m 2>/dev/null
+
+echo "Running deadlock test"
+for i in $(seq 25) ; do
+    yes ./randomfailure.py |head -n1 |$com -n2 2>/dev/null
+done
+
+echo "Running deadlock test with -r"
+for i in $(seq 25) ; do
+    yes ./randomfailure.py |head -n1 |$com -n2 -r 2>/dev/null
+done
